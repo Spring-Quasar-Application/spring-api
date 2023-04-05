02ce1c4b35201c1077dd07511e0b67e010f1ca2f
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-04-05 22:18:40 +0200

    functioning student class(entity, repo, controller)

--- src/main/java/com/spring_api/Main.java
+++ src/main/java/com/spring_api/Main.java
@@ -1,30 +1,12 @@
 package com.spring_api;
 
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @SpringBootApplication
-@RestController
-@RequestMapping("api/v1/customers")
 public class Main {
-
-	private final CustomerRepository customerRepository;
-
-	public Main(CustomerRepository customerRepository) {
-		this.customerRepository = customerRepository;
-	}
 
 	public static void main (String[] args){
 		SpringApplication.run(Main.class, args);
 	}
-
-	@GetMapping
-	public List<Customer> getCustomers() {
-		return customerRepository.findAll();
-	}
 }
+

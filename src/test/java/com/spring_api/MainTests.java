54cb5875151d2fa72b55f771a1331a77b410d089
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-04-06 19:35:42 +0200

    cicd tests

--- src/test/java/com/spring_api/MainTests.java
+++ src/test/java/com/spring_api/MainTests.java
@@ -2,13 +2,14 @@ package com.spring_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@ContextConfiguration(classes = Main.class)
 @SpringBootTest
 class MainTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 
 }

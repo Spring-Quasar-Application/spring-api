b4d02f06e53f369e288c7e00d520bbfc5403a3e2
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-04-09 00:48:32 +0200

    test coverage

--- src/main/java/com/spring_api/Main.java
+++ src/main/java/com/spring_api/Main.java
@@ -8,5 +8,26 @@ public class Main {
 	public static void main (String[] args){
 		SpringApplication.run(Main.class, args);
 	}
+
+	public boolean isPalindrome(String input) {
+
+		if (input == null) {
+			throw new IllegalArgumentException("input shouldn't be null");
+		}
+
+		if (input.equals(reverse(input))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private String reverse(String input) {
+		String rev = "";
+		for (int i = input.length() - 1; i >= 0; i--) {
+			rev = rev + input.charAt(i);
+		}
+		return rev;
+	}
 }
 

5591511554543eadc72daea78b65b9efd66ee52e
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-04-09 15:12:08 +0200

    Update StudentControllerTest.java

--- src/test/java/com/spring_api/controller/StudentControllerTest.java
+++ src/test/java/com/spring_api/controller/StudentControllerTest.java
@@ -7,7 +7,10 @@ import com.spring_api.Main;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -17,8 +20,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = Main.class)
+@SpringBootTest()
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@EnableWebSecurity
+@SpringJUnitConfig
+@ContextConfiguration
 class StudentControllerTest {
 
     @Autowired

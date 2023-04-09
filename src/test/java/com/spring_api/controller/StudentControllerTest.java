2ca925aeecebccc618a9d19e5925511db041e4ee
AlexandruC0909 <35635633+AlexandruC0909@users.noreply.github.com>
2023-04-09 11:59:15 +0200

    Update StudentControllerTest.java

--- src/test/java/com/spring_api/controller/StudentControllerTest.java
+++ src/test/java/com/spring_api/controller/StudentControllerTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = StudentControllerTest.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class StudentControllerTest {
 
